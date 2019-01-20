0bff17712f4eccb6f6609fa049d117df90199d4c
Anish-Agnihotri <contact@anishagnihotri.com>
2019-01-19 23:38:02 -0500

    working on favourite func

--- main.js
+++ main.js
@@ -30,9 +30,30 @@ function fulfillQuery() {
             objects.forEach(function(item, index) {
                  
                 if (`${objects[index].keywords}`.toLowerCase().indexOf(searchbar.val().toLowerCase()) !== -1) {
-                    queryResult += `<tr id='${index}'><td><i class='fa fa-star'></i></td><td>${item.title}</td><td>${item.body}</td></tr>`;
+                    queryResult += `<tr><td><a class='star ${index}' onClick='favourite();'><i class='fa fa-star'></i></a></td><td>${item.title}</td><td>${item.body}</td></tr>`;
                 } 
                 results.html(queryResult);
             });
         })
+}
+
+
+let favouriteList = [];
+
+function favourite() {
+    let attr = $(this).attr('class').replace('star ', '');
+
+    $('.star' + attr).click(function () {  
+        let attr = $(this).attr('class').replace('star ', '');
+        
+        if (favouriteList.includes(attr)) {
+            console.log('already inside');
+
+        }
+        else {
+            console.log('not inside');
+            $('.' + attr).css('color', '#000000 !important');
+            $('.' + attr).parent().parent().clone().appendTo(favourites);
+        }
+    });
 }
